fe4b6fd51d61912292b7b02f54d5803865ac31a8
atul <atulgupta.d796@gmil.com>
2024-08-12 19:11:01 +0530

    12-8-24 v1

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,52 +1,14 @@
 module.exports = [
-  {
-    name: 'strapi::logger',
-    config: {
-      level: 'info',
-    },
-  },
-  {
-    name: 'strapi::errors',
-    config: {},
-  },
-  {
-    name: 'strapi::security',
-    config: {},
-  },
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      origin: [
-        'ttp://localhost:5173',
-        'https://bytebits.vercel.app', // Your frontend URL
-      ],
-      headers: '*',
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    },
-  },
-  {
-    name: 'strapi::poweredBy',
-    config: {},
-  },
-  {
-    name: 'strapi::query',
-    config: {},
-  },
-  {
-    name: 'strapi::body',
-    config: {},
-  },
-  {
-    name: 'strapi::session',
-    config: {},
-  },
-  {
-    name: 'strapi::favicon',
-    config: {},
-  },
-  {
-    name: 'strapi::public',
-    config: {},
-  },
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+
+  
 ];
