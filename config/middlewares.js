6466fd563fad3c844d44bfe1effdc54cafe6d367
atul <atulgupta.d796@gmil.com>
2024-08-12 14:25:09 +0530

    12-8-24 v1

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,51 @@
-module.exports = {
-  settings: {
-    cors: {
+module.exports = [
+  {
+    name: 'strapi::logger',
+    config: {
+      level: 'info',
+    },
+  },
+  {
+    name: 'strapi::errors',
+    config: {},
+  },
+  {
+    name: 'strapi::security',
+    config: {},
+  },
+  {
+    name: 'strapi::cors',
+    config: {
       enabled: true,
       origin: [
         'https://bytebits.vercel.app', // Your frontend URL
       ],
       headers: '*',
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     },
   },
-};
+  {
+    name: 'strapi::poweredBy',
+    config: {},
+  },
+  {
+    name: 'strapi::query',
+    config: {},
+  },
+  {
+    name: 'strapi::body',
+    config: {},
+  },
+  {
+    name: 'strapi::session',
+    config: {},
+  },
+  {
+    name: 'strapi::favicon',
+    config: {},
+  },
+  {
+    name: 'strapi::public',
+    config: {},
+  },
+];
